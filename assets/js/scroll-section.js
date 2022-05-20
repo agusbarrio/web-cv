898a361372c13c2f4b339fa5a71d9c8e5697528a
Agustin Barrio <agusbarrioeastman@gmail.com>
2022-05-20 02:26:39 -0300

    hash sections

--- assets/js/scroll-section.js
+++ assets/js/scroll-section.js
@@ -2,14 +2,12 @@ export default function (sectionLink, header) {
   document.addEventListener('click', (e) => {
     if (e.target.matches(sectionLink)) {
       e.preventDefault();
-      window.hash = e.target.hash;
+      location.hash = e.target.hash;
       let element = document.getElementById(e.target.hash.slice(1));
       let headerHeigth = document
         .querySelector(header)
         .getBoundingClientRect().height;
       let elementPosition = element.offsetTop;
-      console.log(element);
-      console.log(elementPosition);
       window.scroll(0, elementPosition - headerHeigth);
     }
   });
